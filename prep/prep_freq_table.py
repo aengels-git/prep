68bdc3e6ea66fb2c2a678d7021f6821e24fcfd8d
u890640 <alexander.engels@dak.de>
2023-10-16 10:30:43 +0200

    added report options for prep table

--- prep/prep_freq_table.py
+++ prep/prep_freq_table.py
@@ -2,15 +2,19 @@ import polars as pl
 import pandas as pd
 from typing import Union
 
-def prep_freq_table(data:Union[pl.DataFrame,pd.DataFrame], group_vars: Union[list,str]):
+def prep_freq_table(data:Union[pl.DataFrame,pd.DataFrame], 
+                    group_vars: Union[list,str], 
+                    report:bool = False, 
+                    percentage:bool = True, 
+                    digits:int = 2):
     if isinstance(data,pd.DataFrame):
         data = pl.DataFrame(data)
     if isinstance(group_vars,str):
-        return data.groupby(group_vars).agg(
+        tab =  data.groupby(group_vars).agg(
             pl.count()
         ).with_columns(
             (pl.col("count") / pl.sum("count")).alias("proportion")
-        )
+        ).to_pandas()
     else:
         prep_table = data.groupby(group_vars[1:]).agg(
             pl.col(group_vars[0]).apply(lambda s: s.value_counts().to_dict())
@@ -23,4 +27,12 @@ def prep_freq_table(data:Union[pl.DataFrame,pd.DataFrame], group_vars: Union[lis
             prep_table = prep_table.with_columns(
             (pl.col("proportion") / pl.col("proportion").sum().over(var)).alias(f"prop_{var}"),
             )
-        return prep_table.to_pandas()
+        tab = prep_table.to_pandas()
+    if report:
+        if percentage:
+            format_mapping = {col: f"{{:,.{digits}%}}" for col in tab.filter(regex="^prop",axis="columns").columns}
+        else:
+            format_mapping = {col: f"{{:,.{digits}f}}" for col in tab.filter(regex="^prop",axis="columns").columns}
+        for key, value in format_mapping.items():
+            tab[key] = tab[key].apply(value.format)
+    return tab
